ba864ea4b5298c56272491bad057350874ae7ae3
Pete Eichman <peichman14@gmail.com>
2017-01-14 23:56:20 -0500

    basic type input

---
+++ code.rb
@@ -0,0 +1,55 @@
+puts "Welcome to the Ruby Pokemon Type Calculator!"
+puts "What is your pokemon's primary type?"
+types = ["fire", "normal", "fighting", "water", "flying", "grass", "poison", "electric", "ground", "psychic", "rock", "ice", "bug", "dragon", "ghost", "dark", "steel", "fairy"]
+type1 = gets.chomp
+
+while true
+  if types.include?(type1)
+    puts "So it primary type is #{type1}, does it have a secondary type?"
+    break
+  else
+    puts "Please enter a valid Pokemon type from this list:"
+    puts types
+    type1 = gets.chomp
+  end
+end
+response = gets.chomp
+
+while true
+  if response == "yes"
+    puts "What is the secondary type?"
+    type2 = gets.chomp
+    while true
+      if types.include?(type2) && type2 != type1
+        puts "OK, so your pokemon is a #{type1} / #{type2} type."
+        break
+      elsif types.include?(type2) && type2 == type1
+        puts "This is the same as your primary type, are you sure?"
+        type_response = gets.chomp
+        while true
+          if type_response == "yes"
+            puts "OK, so your pokemon is a #{type1} monotype."
+            break
+          elsif type_response == "no"
+            puts "What is the secondary type?"
+            type2 = gets.chomp
+            puts "OK, so your pokemon is a #{type1} / #{type2} type."
+          end
+        end
+        break
+      elsif !types.include?(type2)
+        puts "Please enter a valid Pokemon type from this list:"
+        puts types
+        type1 = gets.chomp
+      end
+      break
+    end
+  elsif response == "no"
+    puts "OK, so your pokemon is a #{type1} monotype."
+    break
+  elsif response != "yes" || response !="no"
+    puts "please use a yes/no response."
+    response = gets.chomp
+  end
+  break
+end
